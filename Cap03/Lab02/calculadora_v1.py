0ea460c84501fefe80189e37909d4565b156295c
Alexandre Marciano <Alexandre@macbooks-MacBook-Pro.local>
2021-03-16 11:44:51 -0300

    Finalizado o Desafio Calculadora py

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -3,7 +3,49 @@
 # Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
 # A solução será apresentada no próximo capítulo!
 # Assista o vídeo com a execução do programa!
+def addNum(x,y):
+    return x + y
+
+def subtNum(x,y):
+    return x - y
+
+def multNum(x,y):
+    return x * y
+
+def divNum(x,y):
+    return x / y
 
 print("\n******************* Python Calculator *******************")
 
+print("\n Selecione o número da operação desejada:")
 
+print("\n 1 - Soma")
+print("\n 2 - Subtração")
+print("\n 3 - Multiplicação")
+print("\n 4 - Divisão")
+
+selectOper = input('Digite sua opção (1/2/3/4):')
+
+primeiroNum = input('Digite o Primeiro Número:')
+
+segundoNum = input('Digite o Segundo Número:')
+
+
+if int(selectOper) == 1:
+    addNum(int(primeiroNum),int(segundoNum))
+    print('%s + %s = %s' %(primeiroNum,segundoNum, addNum(int(primeiroNum),int(segundoNum))))
+
+elif int(selectOper) == 2:
+    subtNum(int(primeiroNum), int(segundoNum))
+    print('%s + %s = %s' %(primeiroNum,segundoNum, subtNum(int(primeiroNum),int(segundoNum))))
+
+elif int(selectOper) == 3:
+    multNum(int(primeiroNum), int(segundoNum))
+    print('%s + %s = %s' %(primeiroNum,segundoNum, multNum(int(primeiroNum),int(segundoNum))))
+
+elif int(selectOper) == 4:
+    divNum(int(primeiroNum), int(segundoNum))
+    print('%s + %s = %s' %(primeiroNum,segundoNum, divNum(int(primeiroNum),int(segundoNum))))
+
+else:
+    print("Operação Inválida")
